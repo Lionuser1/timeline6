59dac84c7e367b96b53f3cfa3135cfc08e104393
Lionuser <lionuser@codelion.net>
2014-02-19 09:46:41 +0000

    라우트 지정하기

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Timeline::Application.routes.draw do
   get "user/signup"
 	post "user/signup_complete"
 	get "user/login"
+	post "user/login_complete"
+	get "user/logout"
   get "wall/write"
 	post "wall/write_complete"
  	get "wall/posts"
