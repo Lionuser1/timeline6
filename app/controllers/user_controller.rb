feaaa595f570a392632c9ab501b15a28fc5cdde2
Lionuser <lionuser@codelion.net>
2014-02-19 09:24:17 +0000

    회원가입 정보 저장 및 화면 전환 기능 추가하기

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,22 @@
 class UserController < ApplicationController
   def signup
   end
+	
+	def signup_complete
+		u = User.new
+		u.username = params[:username]
+		if params[:password] == params[:retype_password]
+			u.password = params[:password]
+			if u.save
+				flash[:alert] = "Successfully signed up."
+				redirect_to "/wall/posts"
+			else
+				flash[:alert] = u.errors.values.flatten.join(' ')
+				redirect_to :back
+			end
+		else
+			flash[:alert] - "Password doesn't match."
+			redirect_to :back
+		end
+	end
 end
